fd4ad4686321fcc106d37af6f2c2a624240e575c
skevprog <ksoruco@r1group.co>
2018-09-08 13:05:17 -0300

    Initial logic for displaying styles when the sound is playing

--- js/app.js
+++ js/app.js
@@ -2,13 +2,20 @@
 window.addEventListener('keydown', (e) => {
 
    const key = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+   const soundKey = document.querySelector(`.string[data-key="${e.keyCode}"]`);
+
+
+
    if (!key) return;
 
    if (key.paused) {
       key.play();
+      soundKey.classList.add('active')
    } else {
       key.pause()
       key.currentTime = 0;  //restart sound
+      soundKey.classList.remove('active')
    }
 
 })
+
